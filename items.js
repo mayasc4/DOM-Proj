b22ee469c7bcbb49bc375cbfe7f62f20ed9b554f
Maya Schuster <mayasc@wix.com>
2015-06-29 18:50:56 +0300

    first commit

---
+++ items.js
@@ -0,0 +1,48 @@
+
+var ITEMS = [
+    {
+        "id": 4,
+        "name": "Item 4",
+        "price": 34
+    },
+    {
+        "id": 2,
+        "name": "Item 2",
+        "price": 31
+    },
+    {
+        "id": 3,
+        "name": "Item 3",
+        "price": 98
+    },
+    {
+        "id": 4,
+        "name": "Item 4",
+        "price": 98
+    },
+    {
+        "id": 5,
+        "name": "Item 5",
+        "price": 98
+    },
+    {
+        "id": 6,
+        "name": "Item 6",
+        "price": 98
+    },
+    {
+        "id": 7,
+        "name": "Item 7",
+        "price": 98
+    },
+    {
+        "id": 8,
+        "name": "Item 8",
+        "price": 98
+    },
+    {
+        "id": 9,
+        "name": "Item 9",
+        "price": 98
+    }
+]
