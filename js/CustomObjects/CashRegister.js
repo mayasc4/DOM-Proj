67960ebb82eee00e3b4d6d875eb88926da7e7d31
Maya Schuster <mayasc@wix.com>
2015-07-22 10:44:07 +0300

    added .gitignore and lodash on something

--- js/CustomObjects/CashRegister.js
+++ js/CustomObjects/CashRegister.js
@@ -13,12 +13,21 @@ App.CashRegister = (function () {
     function calculateTotalCost(couponCode) {
         var tempTotal = 0;
         var maxCost = 0;
-        _.forEach(_.keys(productsInCart), function(productId){
-            var product = App.Products.getProductById(productId);
-            var price = product.onSale ? product.priceAfterSale : product.intPrice;
-            maxCost = maxCost < price ? price : maxCost;
-            tempTotal += price * productsInCart[productId];
+
+        // TODO make this more pretty
+
+        var productElements = _.filter(App.Products.getAllProducts(), function(product) {
+            var retVal = _.includes(Object.keys(productsInCart), product.id.toString());
+            return retVal;
         });
+
+
+        tempTotal = _.reduce(productElements, function(acc, curr) {
+            var price = curr.onSale ? curr.priceAfterSale : curr.intPrice;
+            return acc + ( price * productsInCart[curr.id]);
+        }, 0);
+
+        maxCost = _.max(_.pluck(productElements, 'intPrice'));
 
         totalCost = tempTotal;
 
